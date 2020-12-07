0a3ec5e9861651a3b89f87481e26ca90d804d26b
KSExtrez <extrez10@gmail.com>
2020-12-07 17:02:44 -0600

    Second try

--- Day1/reportRepair.py
+++ Day1/reportRepair.py
@@ -1,14 +1,31 @@
+from typing import Hashable
+
 # Big O(n*n) works, but too slow
-def expensive(numbers):
+def firstTry(numbers):
     for number in numbers:
         for number2 in numbers:
             if number + number2 == 2020:
                 print(number)
                 print(number2)
                 print(number * number2)
+                break
             pass
         pass
 
+# Big O(n), now looks good 😉
+def secondTry(numbers):
+    dic = {}
+    for number in numbers:
+        dic[number] = True
+
+        toFind = 2020 - number
+
+        if dic.get(toFind):
+            print(number)
+            print(toFind)
+            print(number * toFind)
+            break
+        pass
 
 
 numbers = [
@@ -19,4 +36,4 @@ numbers = [
     675,
     1456
     ]
-expensive(numbers)
+firstTry(numbers)
